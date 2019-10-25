ac20140a186a2c5b7f9b51a23d970bfe227b71e3
Mick F <mick@mickf.net>
2019-10-25 11:17:15 +0200

    feat(server): add logs when receiving a message

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
 const http = require("http");
 const express = require("express");
 const MessagingResponse = require("twilio").twiml.MessagingResponse;
+const bodyParser = require("body-parser");
+
+const port = process.env.PORT || 5000;
 
 const app = express();
-const port = process.env.PORT || 5000;
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => res.send("Welcome to poc-twilio"));
 
 app.post("/sms", (req, res) => {
-  const twiml = new MessagingResponse();
+  if (req.body == null || req.body.Body == null) {
+    throw Error("Invalid message");
+  }
 
-  twiml.message("The Robots are coming! Head for the hills!");
+  console.log(`New incoming SMS received: ${req.body.Body}`);
 
-  res.writeHead(200, { "Content-Type": "text/xml" });
-  res.end(twiml.toString());
+  res.writeHead(204);
+  res.end();
 });
 
 http.createServer(app).listen(port, () => {
