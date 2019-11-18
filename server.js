fcf952f3338147dc563f410862d6ce880db9a627
Mick F <mick@mickf.net>
2019-11-18 19:33:58 +0100

    feat: deactivate Twilio middleware

--- server.js
+++ server.js
@@ -15,29 +15,32 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => res.send("Hello!"));
 
-app.post("/sms", twilio.webhook({ validate: true }), (req, res) => {
-  if (req.body == null || req.body.Body == null) {
-    throw Error("Invalid message");
+app.post(
+  "/sms",
+  /* twilio.webhook({ validate: true }), */ (req, res) => {
+    if (req.body == null || req.body.Body == null) {
+      throw Error("Invalid message");
+    }
+
+    const message = req.body.Body;
+    console.log(`New incoming SMS received: ${message}`);
+
+    res.writeHead(204);
+    res.end();
+
+    latestMessage = message;
+
+    if (process.env.SLACK_WEBHOOK_URL) {
+      axios
+        .post(process.env.SLACK_WEBHOOK_URL, {
+          text: message
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    }
   }
-
-  const message = req.body.Body;
-  console.log(`New incoming SMS received: ${message}`);
-
-  res.writeHead(204);
-  res.end();
-
-  latestMessage = message;
-
-  if (process.env.SLACK_WEBHOOK_URL) {
-    axios
-      .post(process.env.SLACK_WEBHOOK_URL, {
-        text: message
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
-  }
-});
+);
 
 app.get("/latest", (req, res) => {
   if (
